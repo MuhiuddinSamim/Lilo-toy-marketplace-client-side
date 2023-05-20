035bc7f0b0f67edff5cf8bddbc3bfe302395b998
Muhiuddinsamim <Muhiuddinsamim007@gmail.com>
2023-05-20 10:45:47 +0600

    env file for config

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import loginImg from '../../assets/Data_security_05.jpg'
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [success,setSuccess]=useState('');
+    const[error,setError]=useState('');
+
+
+       const handleLogIn=(event)=>{
+        event.preventDefault();
+        setError('');
+       setSuccess('');
+        const form=event.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        console.log(email,password)
+        if(!/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(password)){
+            setError('Minimum six characters, at least one letter, one number and one special character:');
+            return;
+        }
+
+    }
+
+
+
+
     return (
      <div className='md:flex'>
         <div className='md:w-1/2'>
@@ -11,7 +33,7 @@ const Login = () => {
         <div className=' md:w-1/2'>
             <div className='py-36'>
                 <h1 className='text-center text-primary text-3xl font-bold'>Log in</h1>
-               <form >
+               <form onSubmit={handleLogIn} >
           <div className="card-body">
         <div className="form-control">
           <label className="label">
@@ -31,9 +53,11 @@ const Login = () => {
           <button className="btn btn-primary w-24">Login</button>
         </div>
         <div className="text-grey-dark text-center mt-6">
-            <p>  Already Have an Account? 
+            <p>   New to Lilo? 
             <Link to='/signUp' className="no-underline link-hover border-b text-xl text-violet-500 font-bold">
                  Sign up</Link>.</p>
+                 <p className='text-red-500'>{error}</p>
+                 <p className='text-primary text-2xl'>{success}</p>
          
         </div>
       </div>
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import loginImg from '../../assets/2942004.jpg'
 
 
 const SignUp = () => {
+    const [success,setSuccess]=useState('');
+    const[error,setError]=useState('');
+
+
+    const handleSignUp=(event)=>{
+        event.preventDefault();
+        setError('');
+       setSuccess('');
+        const form=event.target;
+        const name=form.name.value;
+        const photo=form.photo.value;
+        const email=form.email.value;
+        const password=form.password.value;
+        if(!/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(password)){
+            setError('Minimum six characters, at least one letter, one number and one special character in password');
+            return;
+        }
+        console.log(name,photo,email,password)
+
+    }
+
+
+
+
+
+
+
+
     return (
         <div className='md:flex p-5'>
        
         <div className=' md:w-1/2'>
             <div className='py-20'>
                 <h1 className='text-center text-primary text-3xl font-bold'>Sign Up Now</h1>
-               <form >
+               <form  onSubmit={handleSignUp}>
           <div className="card-body">
         <div className="form-control">
           <label className="label">
@@ -42,9 +70,11 @@ const SignUp = () => {
           <button className="btn btn-primary w-24">Sign up</button>
         </div>
         <div className="text-grey-dark text-center mt-6">
-            <p>  New to Lilo? 
+            <p>Already Have an Account?  
             <Link to='/login' className="no-underline link-hover border-b text-xl text-violet-500 font-bold">
                  Log In</Link>.</p>
+                  <p className='text-red-500'>{error}</p>
+                 <p className='text-primary text-2xl'>{success}</p>
          
         </div>
       </div>
